a579a34e04c134967ba09951da8ad57dc9063084
Kenjirl <kenjiro442@gmail.com>
2025-08-06 21:23:37 +0800

    Update 6 August

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -9,4 +9,30 @@ $(document).ready(function() {
         $(this).toggleClass('active');
         $(this).closest('.dropdown-btn-wrapper').next('.dropdown-content').toggleClass('active');
     });
+
+    $(".click-to-copy").on("click", function (e) {
+        e.preventDefault();
+
+        const textToCopy = $(this).data("copy");
+
+        navigator.clipboard.writeText(textToCopy).then(() => {
+            Toastify({
+                text: "Disalin: " + textToCopy,
+                duration: 3000,
+                close: true,
+                gravity: "bottom",
+                style: {
+                    background: "#4CAF50",
+                    fontSize: "12px",
+                    maxWidth: "unset",
+                    borderRadius: "4px",
+                    padding: "10px",
+                    boxShadow: "0 2px 10px rgba(0,0,0,0.2)",
+                    textAlign: "right",
+                }
+            }).showToast();
+        }).catch((err) => {
+            console.error("Gagal menyalin:", err);
+        });
+    });
 });
